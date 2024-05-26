571f485357f2b41bb15cfbb9617f12567a6fe7b7
Marcos Paulo <mp.matos25@gmail.com>
2024-05-26 11:26:57 -0300

    feat: create useAPI hook

---
+++ src/hooks/useApi.hook.ts
@@ -0,0 +1,71 @@
+'use client'
+
+import { News, NewsHeadline, NewsSecondary } from '@/domain';
+import Api from '@/services/Api.service';
+import { useState } from 'react';
+
+type Response<T> = {
+  loading: boolean;
+  error: string | null;
+  data: T;
+}
+
+const useApi = (baseURL: string) => {
+  const [headline, setHeadline] = useState<Response<NewsHeadline[]>>({} as Response<NewsHeadline[]>);
+  const [secondary, setSecondary] = useState<Response<NewsSecondary[]>>({} as Response<NewsSecondary[]>);
+  const [show, setShow] = useState<Response<News>>({} as Response<News>);
+
+  const api = new Api(baseURL);
+
+  const getHeadline = async () => {
+    setHeadline({ loading: true, error: null, data: {} as NewsHeadline[] });
+    try {
+      const data = await api.headline();
+
+      setHeadline((prevState) => ({ ...prevState, data: data,}));
+    } catch (err) {
+      setHeadline((prevState) => ({ ...prevState, error: (err as Error).message}));
+    } finally {
+      setHeadline((prevState) => ({ ...prevState, loading: false,}));
+    }
+  };
+
+  const getSecondary = async () => {
+    setSecondary({ loading: true, error: null, data: {} as NewsSecondary[] });
+    try {
+      const data = await api.secondary();
+
+      setSecondary((prevState) => ({ ...prevState, data: data,}));
+    } catch (err) {
+      setSecondary((prevState) => ({ ...prevState, error: (err as Error).message}));
+    } finally {
+      setSecondary((prevState) => ({ ...prevState, loading: false,}));
+    }
+  };
+
+  const getShow = async (id: string) => {
+    setShow({ loading: true, error: null, data: {} as News });
+    try {
+      const data = await api.show(id);
+
+      setShow((prevState) => ({ ...prevState, data: data,}));
+    } catch (err) {
+      setShow((prevState) => ({ ...prevState, error: (err as Error).message}));
+    } finally {
+      setShow((prevState) => ({ ...prevState, loading: false,}));
+    }
+  };
+
+  const test = async () => {
+    try {
+      await api.test();
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  return { getHeadline, getSecondary, getShow, test, headline, secondary, show };
+};
+
+export default useApi;
