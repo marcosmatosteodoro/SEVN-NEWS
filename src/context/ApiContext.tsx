c47342ffdfd17662cf8492f0f779f06a266ada26
Marcos Paulo <mp.matos25@gmail.com>
2024-05-26 11:41:32 -0300

    feat: create ApiContext

---
+++ src/context/ApiContext.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React, { createContext, useContext } from 'react';
+
+type ApiContextType = {
+  apiUrl: string;
+};
+
+const ApiContext = createContext<ApiContextType | undefined>(undefined);
+
+export const ApiProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? '';
+
+  return (
+    <ApiContext.Provider value={{ apiUrl }}>
+      {children}
+    </ApiContext.Provider>
+  );
+};
+
+export const useApiContext = (): ApiContextType => {
+  const context = useContext(ApiContext);
+  if (!context) {
+    throw new Error('useApi must be used within an ApiProvider');
+  }
+  return context;
+};
