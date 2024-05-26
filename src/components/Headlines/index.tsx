ab2d6b3c41cfb11c21c43b29ef0addee8a38381c
Marcos Paulo <mp.matos25@gmail.com>
2024-05-26 01:40:41 -0300

    feat: create Headlines component

---
+++ src/components/Headlines/index.tsx
@@ -0,0 +1,23 @@
+import { NewsHeadline } from "@/domain";
+import { Headline } from "../Headline";
+import { SecondaryHeadline } from "../SecondaryHeadline";
+import style from "./style.module.scss";
+
+type HeadlinesProps = {
+  headlines: NewsHeadline[]
+}
+
+export function Headlines({ headlines }: HeadlinesProps) {
+  return (
+    <div className={style.container}>
+      <Headline headline={headlines[0]}/>
+
+      <div className={style.secondary}>
+        {headlines.slice(1).map((headline, index) => (
+          <SecondaryHeadline key={index} headline={headline} />
+        ))}
+      </div>
+
+    </div>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export * from "./Advertising";
 export * from "./CategoryText";
 export * from "./Header";
 export * from "./Headline";
+export * from "./Headlines";
 export * from "./icons";
 export * from "./PageContent";
 export * from "./SecondaryHeadline";
