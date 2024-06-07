896e2d1291547ce982c9878c7461aeea51517b0c
Marcos Paulo <mp.matos25@gmail.com>
2024-06-07 00:37:37 -0300

    test: create PageContent.test

---
+++ src/components/PageContent/PageContent.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import { PageContent } from './';
+
+describe('PageContent', () => {
+  it('should render children and header without back button by default', () => {
+    render(
+      <PageContent>
+        <div data-testid="test-child">Test Child</div>
+      </PageContent>
+    );
+
+    const testChild = screen.getByTestId('test-child');
+    expect(testChild).toBeInTheDocument();
+
+    const header = screen.getByRole('banner');
+    expect(header).toBeInTheDocument();
+    expect(screen.queryByTestId('back-button')).not.toBeInTheDocument(); // Verifica se o botão de voltar não está presente
+  });
+
+  it('should render children and header with back button when backButton prop is true', () => {
+    render(
+      <PageContent backButton={true}>
+        <div data-testid="test-child">Test Child</div>
+      </PageContent>
+    );
+
+    const testChild = screen.getByTestId('test-child');
+    expect(testChild).toBeInTheDocument();
+
+    const header = screen.getByRole('banner');
+    expect(header).toBeInTheDocument();
+    expect(screen.getByTestId('back-button')).toBeInTheDocument(); // Verifica se o botão de voltar está presente
+  });
+});
